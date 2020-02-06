c67a410dc5b34f74295a1ad4bb41d6d02fc192ba
Matheus Alencar <malencar@pm.me>
2020-02-06 16:50:27 -0300

    fix: incorrect parameter name

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const db = level('./db')
 const twitterClient = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
+  access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
